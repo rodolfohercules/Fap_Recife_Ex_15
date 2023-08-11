4d548e5a52fc8df8c99fcbbe64598a2973f929fd
Rodolfo Hercule <71905257+rodolfohercules@users.noreply.github.com>
2023-08-11 19:49:27 -0300

    firstCommit

---
+++ app.js
@@ -0,0 +1,16 @@
+const names = ['Rodolfo', 'Miguel', 'Elitiziane', 'Luiz', 'Edileuza', 'Eloiza', 'Josefa', 'Carlos', 'Merycles', 'Rayane']
+
+const age = [34, 5, 27, 45, 42, 21, 61, 51, 25, 32]
+
+const color = ['Preto', 'Verde', 'Azul', 'Rosa', 'Branco', 'Roxo', 'Amarelo' , 'Cinza', 'Vermelho', 'Laranja']
+
+console.log(`Lista original de Nomes:  ${names}`)
+console.log(`Lista original das Idades: ${age}`)
+console.log(`Lista original das cores: ${color}`)
+
+age[4] = 15;
+color[1] = 'Pink';
+
+console.log(`Lista original de Nomes:  ${names}`)
+console.log(`Lista original das Idades: ${age}`)
+console.log(`Lista original das cores: ${color}`)
